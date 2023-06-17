f4bbb4a6ddd7082b66fdc41a4eb48d45f7691db2
Pelahiia Khalishkhova <pelahiia.kh@gmail.com>
2023-06-17 22:06:43 +0200

    cards location

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,22 +4,22 @@ export const Header: React.FC = () => {
   return(
     <div className="header">
       <div className="header-nav">
-        <a href="/" className="header-nav-item">
+        <a href="" className="header-nav-item">
           <img src={logo} alt="Logo" className="logo" />
         </a>
-        <a href="/" className="header-nav-item">
+        <a href="" className="header-nav-item">
           Start
         </a>
-        <a href="/" className="header-nav-item">
+        <a href="" className="header-nav-item">
           Lösungen
         </a>
-        <a href="/" className="header-nav-item">
+        <a href="" className="header-nav-item">
           Kontakt
         </a>
-        <a href="/" className="header-nav-item">
+        <a href="" className="header-nav-item">
           Über uns
         </a>
-        <a href="/" className="header-nav-item">
+        <a href="" className="header-nav-item">
           Impressum
         </a>
       </div>
